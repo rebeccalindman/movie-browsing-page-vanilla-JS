2dd1b75e4f80784200d896e435a6846008ba6224
rebeccalindman <rebeccalindman@icloud.com>
2024-12-18 10:19:41 +0100

    testing API

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # movie-browsing-page-vanilla-JS
+
+
+https://developer.themoviedb.org/docs/getting-started
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const API_KEY = "1d8a8c8a";
+const API_KEY_omdb = "1d8a8c8a"; //omdbapi.com
+const API_KEY_tmdb = "6369fcc46c83ecd475d3f734321f2a0b"; //themoviedb.org
 
-const apiUrl = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+const apiUrl = `http://www.omdbapi.com/?apikey=${API_KEY_omdb}&`;
+const apiUrl_MovieDB = ``;
 
 
 
-const searchInput = "harry"; //!test
+let searchInput = "harry"; //!test
 
 //api search
-let searchUrl = apiUrl += `s=${searchInput}`;
+let searchUrl = apiUrl + `s=${searchInput}`;
 
 
 // fetch data from the api
-const getMovies = async(url) => {   
+let getMovies = async(url) => {   
     const response = await fetch(url);
     const data = await response.json();
     return data;
