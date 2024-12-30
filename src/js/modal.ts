50f5813335193e28e63ce0df4c1c631342e98710
rebeccalindman <rebeccalindman@icloud.com>
2024-12-30 18:19:03 +0100

    added favorite icon to modal, without effect though

--- src/js/modal.ts
+++ src/js/modal.ts
@@ -1,13 +1,19 @@
 import { Movie } from "./types.ts";
+import { isFavorite } from "./utils.ts";
 
 
   /* TODO replace with star icon and heart icon */
   export function createMovieModal(movie: Movie) {
+
+ // Check if the movie is a favorite
+  const isFavorited = isFavorite(movie.id);
+  const favoriteClass = isFavorited ? "loved" : "";
+
     const movieModal = document.createElement('div');
     movieModal.classList.add('movie-modal');
     movieModal.innerHTML = `
     <button type="button" class="movie-modal-close">X</button>
-    <div class="movie-modal-backdrop" style="background-image: url('https://image.tmdb.org/t/p/w500/${movie.backdrop_path}')" aria-label="${movie.title} movie backdrop">
+    <div class="movie-modal-backdrop" style="background: url('https://image.tmdb.org/t/p/w500/${movie.backdrop_path}') lightgray 50% / cover no-repeat" aria-label="${movie.title} movie backdrop">
       <svg class ="ux-shape-divider" viewBox="0 0 1000 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
                     <path class="ux-shape-fill dark" d="M0 0C0 0 200 50 500 50C800 50 1000 0 1000 0V101H0V1V0Z"></path>
                 </svg>
@@ -21,7 +27,11 @@ import { Movie } from "./types.ts";
             <p class="movie-modal-rating">${movie.vote_average}/10</p> 
             <p class="vote-count">(${movie.vote_count} votes)</p> 
         </div>
-        <button type="button" class="love-button">❤️</button> 
+        <button class="love-button ${favoriteClass}" data-movie-id="${movie.id}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path d="M12.62 20.8101C12.28 20.9301 11.72 20.9301 11.38 20.8101C8.48 19.8201 2 15.6901 2 8.6901C2 5.6001 4.49 3.1001 7.56 3.1001C9.38 3.1001 10.99 3.9801 12 5.3401C13.01 3.9801 14.63 3.1001 16.44 3.1001C19.51 3.1001 22 5.6001 22 8.6901C22 15.6901 15.52 19.8201 12.62 20.8101Z" fill="white" fill-opacity="0.4"/>
+        </svg>
+      </button> 
     </section>
     <section class="information-container">
         <ul class="movie-genres">
